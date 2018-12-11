56435fc551187aa1b85bf68773b36b7fd1b92e9f
dohong <workthread.start@gmail.com>
2018-12-11 14:28:52 +0900

    초기화

---
+++ DataHelper/JsonDataExtensions.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DataHelper
+{
+    /// <summary>
+    /// DataRow를 JObject로 변경 시키는 로직 설정
+    /// </summary>
+    /// <param name="obj">변경 될 JObject 객체, DataRow의 컬럼과 1:1 매칭되지 않는 경우 수동으로 넣어줄 때 사용한다</param>
+    /// <param name="column">컬럼</param>
+    /// <param name="data">데이터</param>
+    /// <param name="row">데이터 변경 시 같은 DataRow의 다른 데이터를 사용할 경우 사용한다.</param>
+    /// <returns>JObject 변경 로직으로 전달 될 결과, 컬럼 사용 유무, JObject에 사용 될 컬럼(Key), JObject에 사용 될 데이터(Value)</returns>
+    public delegate Tuple<bool, string, JToken> JsonDataValidator(JObject obj, string column, object data, DataRow row);
+
+    public static class JsonDataExtensions
+    {
+        static JsonDataExtensions()
+        {
+            DefaultValidator = (obj, column, data, row) =>
+            {
+                return new Tuple<bool, string, JToken>(true, column, data.ToString());
+            };
+        }
+
+        /// <summary>
+        /// 기본 변경 로직 설정
+        /// </summary>
+        public static JsonDataValidator DefaultValidator { get; set; }
+
+        /// <summary>
+        /// DataRow를 JObject로 변환
+        /// </summary>
+        /// <param name="dataRow"></param>
+        /// <param name="validator">변환 로직</param>
+        /// <returns></returns>
+        public static JObject ToJObject(this DataRow dataRow, JsonDataValidator validator)
+        {
+            JObject result = new JObject();
+            foreach (DataColumn column in dataRow.Table.Columns)
+            {
+                Tuple<bool, string, JToken> data = validator(result, column.ColumnName, dataRow[column.ColumnName], dataRow);
+                if (data.Item1)
+                {
+                    result[data.Item2] = data.Item3;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        // DataRow를 JObject로 변환
+        /// </summary>
+        /// <param name="dataRow"></param>
+        /// <returns></returns>
+        public static JObject ToJObject(this DataRow dataRow)
+        {
+            return dataRow.ToJObject(DefaultValidator);
+        }
+
+        /// <summary>
+        /// DataTable을 JArray로 변환
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <param name="validator">변환 로직</param>
+        /// <returns></returns>
+        public static JArray ToJArray(this DataTable dataTable, JsonDataValidator validator)
+        {
+            JArray result = new JArray();
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                result.Add(dataRow.ToJObject(validator));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// DataTable을 JArray로 변환
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        public static JArray ToJArray(this DataTable dataTable)
+        {
+            return dataTable.ToJArray(DefaultValidator);
+        }
+
+        /// <summary>
+        /// DataRow[]을 JArray로 변환
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <param name="validator">변환 로직</param>
+        /// <returns></returns>
+        public static JArray ToJArray(this DataRow[] dataRows, JsonDataValidator validator)
+        {
+            JArray result = new JArray();
+            foreach (DataRow dataRow in dataRows)
+            {
+                result.Add(dataRow.ToJObject(validator));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// DataRow[]을 JArray로 변환
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        public static JArray ToJArray(this DataRow[] dataRows)
+        {
+            return dataRows.ToJArray(DefaultValidator);
+        }
+    }
+}
---
+++ DataHelper/XmlNodeExtensions.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DataHelper
+{
+    /// <summary>
+    /// Xml 유틸 클래스
+    /// </summary>
+    public static class XmlNodeExtensions
+    {
+        /// <summary>
+        /// Xml 노드를 테이블로 전환합니다.
+        /// AddData 함수를 이용합니다.
+        /// </summary>
+        /// <param name="self">노드</param>
+        /// <param name="xpath">타겟 XPath</param>
+        /// <param name="depth">몇 번째 depth까지 탐색하여 테이블을 생성할지 표시합니다.</param>
+        /// <returns></returns>
+        public static DataTable ToDataTable(this XmlNode self, string xpath, int depth = 1)
+        {
+            DataTable table = new DataTable();
+            foreach (XmlNode item in self.SelectNodes(xpath))
+            {
+                DataRow row = table.NewRow();
+                row.AddData(item, string.Empty, 1, depth);
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        /// <summary>
+        /// 컬럼명을 "-" 기준으로 줄여줍니다.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="depth"></param>
+        /// <param name="separator"></param>
+        public static void ShortenColumnName(this DataTable self, int depth = 1, string separator = "-")
+        {
+            foreach (DataColumn item in self.Columns)
+            {
+                for (int i = 0; i < depth; i++)
+                {
+                    int first = item.ColumnName.IndexOf(separator);
+                    if (first >= 0)
+                    {
+                        item.ColumnName = item.ColumnName.Substring(first + 1, item.ColumnName.Length - first - 1);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// DataRow에 Node의 데이터를 추가합니다.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="node"></param>
+        /// <param name="parent"></param>
+        /// <param name="index"></param>
+        /// <param name="depth"></param>
+        public static void AddData(this DataRow self, XmlNode node, string parent, int index, int depth)
+        {
+            if (node.Attributes != null)
+            {
+                foreach (XmlAttribute attribute in node.Attributes)
+                {
+                    string name = node.Name;
+                    if (index > 1)
+                    {
+                        name = string.Format("{0}-{1}", name, index);
+                    }
+                    string columnName = string.Empty.Equals(parent) ? string.Format("{1}-{2}", name, attribute.Name) : string.Format("{0}-{1}-{2}", parent, name, attribute.Name);
+                    if (!self.Table.Columns.Contains(columnName))
+                    {
+                        self.Table.Columns.Add(columnName);
+                    }
+                    self[columnName] = attribute.Value.ToDBValue();
+                }
+            }
+            if (node.ChildNodes.Count > 0)
+            {
+                if (depth > 0)
+                {
+                    parent = string.Empty.Equals(parent) ? node.Name : string.Format("{0}-{1}", parent, node.Name);
+                    List<string> childList = new List<string>();
+                    foreach (XmlNode child in node.ChildNodes)
+                    {
+                        string childName = child.Name;
+                        childList.Add(child.Name);
+                        index = childList.Count(c => c == child.Name);
+                        self.AddData(child, parent, index, depth - 1);
+                    }
+                }
+            }
+            else if (!string.IsNullOrEmpty(node.InnerText))
+            {
+                string name = node.Name;
+                string columnName = string.Empty.Equals(parent) ? name : string.Format("{0}-{1}", parent, name);
+                if (!self.Table.Columns.Contains(columnName))
+                {
+                    self.Table.Columns.Add(columnName);
+                }
+                self[columnName] = node.InnerText.ToDBValue();
+            }
+        }
+
+        /// <summary>
+        /// object를 DB데이터 형태로 변환합니다.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static object ToDBValue(this object value)
+        {
+            if (value == null)
+            {
+                return DBNull.Value;
+            }
+            else if (value is string && string.IsNullOrEmpty((string)value))
+            {
+                return DBNull.Value;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
